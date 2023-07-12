830e7c3a8a40b63cae84b4d8fb6efa3fa97f805f
Kiran-KumarM <kiran.kumar.msingh@gmail.com>
2023-07-12 11:35:27 +0530

    trigger example one

--- scrollTrigger.js
+++ scrollTrigger.js
@@ -5,7 +5,7 @@ var t1=gsap.timeline({
         trigger:'#box3',
         start:'center center', // top center bottom 20px 20%
         end:'center 20%',
-        markers:true,
+        //markers:true,
        // scrub:true,//animation lock with scroll bar
        scrub:true,
        //pin:true, // element maintains it position on scroll
@@ -30,3 +30,24 @@ t1.to('.box3',{
     x:500
 })
 
+//------------------ EXAMPLE 1 -------------------------
+
+/* Panels */
+panelsContainer = document.querySelector(".panel_wrappr");
+const panels = gsap.utils.toArray(".panel_wrappr .panel");
+tween = gsap.to(panels, {
+	xPercent: -100 * ( panels.length - 1 ),
+	ease: "none",
+	scrollTrigger: {
+		trigger: ".panel_wrappr",
+		pin: true,
+		start: "top top",
+		scrub: 1,
+		snap: {
+			snapTo: 1 / (panels.length - 1),
+			inertia: false,
+			duration: {min: 0.1, max: 0.1}
+		},
+		end: () =>  "+=" + (panelsContainer.offsetWidth - innerWidth)
+	}
+});
